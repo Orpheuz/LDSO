9672bec3a2f2d29b4ffb81ce49c09db1fb28fe9d
Leonardo Faria <leonardofaria94@gmail.com>
2015-11-05 15:25:15 +0000

    merge e cenas

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151104233630) do
+ActiveRecord::Schema.define(version: 20151105150227) do
 
-  create_table "bookmarks", force: true do |t|
+  create_table "average_caches", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "avg",           null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
-    t.integer  "recipe_id"
   end
-
-  add_index "bookmarks", ["recipe_id"], name: "index_bookmarks_on_recipe_id"
-  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "ingredient_recipe_associations", force: true do |t|
     t.integer  "recipe_id"
@@ -39,16 +38,46 @@ ActiveRecord::Schema.define(version: 20151104233630) do
     t.datetime "updated_at"
   end
 
+  create_table "overall_averages", force: true do |t|
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "overall_avg",   null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "rates", force: true do |t|
+    t.integer  "rater_id"
+    t.integer  "rateable_id"
+    t.string   "rateable_type"
+    t.float    "stars",         null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rates", ["rateable_id", "rateable_type"], name: "index_rates_on_rateable_id_and_rateable_type"
+  add_index "rates", ["rater_id"], name: "index_rates_on_rater_id"
+
+  create_table "rating_caches", force: true do |t|
+    t.integer  "cacheable_id"
+    t.string   "cacheable_type"
+    t.float    "avg",            null: false
+    t.integer  "qty",            null: false
+    t.string   "dimension"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rating_caches", ["cacheable_id", "cacheable_type"], name: "index_rating_caches_on_cacheable_id_and_cacheable_type"
+
   create_table "recipes", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
-
-  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
 
   create_table "steps", force: true do |t|
     t.integer  "recipe_id"
