1af4ffe389bfb18401035d2a0ba2ff3bfba38d41
ZPaulo <jose.p.s.olive@gmail.com>
2015-11-17 02:23:08 +0000

    search "working", doesnt work on content in seed.rb

--- app/assets/javascripts/search.js.coffee
+++ app/assets/javascripts/search.js.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+
+$(document).ready ->
+  $(".search-choice").on "click",(event) ->
+    oldVal = $('#search-choice-btn').html()
+    val =  $('#' + event.target.id).html()
+    $('#search-choice-btn').html val
+    $('#' + event.target.id).html oldVal
+    $('#SCN').attr 'value', val
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,21 +1,42 @@
 class SearchController < ApplicationController
 
   def index
+    if params[:SCN].present?
+      if params[:SCN] == 'Name'
+        @search = Recipe.search do
+          fulltext params[:search] do
+            fields(:name)
+          end
+          if(params[:number])
+            without(:time).greater_than(params[:number][0])
+          end
+          params.delete :number
+          order_by :time, :asc
+          paginate :per_page => 15
+        end
+        @recipes = @search.results
+      else
+        if params[:SCN] == 'Tag'
+          @search = Tag.search do
+            fulltext params[:search] do
+              fields(:name)
+            end
+            paginate :per_page => 15
+          end
+          @tags = @search.results
 
-    @search = Recipe.search do
-      fulltext params[:search] do
-        fields(:name)
+          recipes = Array.new
+          @tags.each do |tag|
+            RecipeTagAssociation.where(tag_id: tag.id).each do |rta|
+              recipes.push(Recipe.find(rta.recipe_id))
+            end
+          end
+
+          @recipes = recipes
+        end
       end
-      if(params[:number])
-        without(:time).greater_than(params[:number][0])
-      end
-      params.delete :number
-      order_by :time, :asc
-      paginate :per_page => 15
+    else
+      @recipes = Recipe.all
     end
-
-
-    @recipes = @search.results
   end
-
 end
