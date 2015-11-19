a9904fee1a19983426be29766a06838b544d7fb3
vania <ei12037@fe.up.pt>
2015-11-19 11:14:49 +0000

    bookmark bug fixed

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -106,6 +106,8 @@ class RecipesController < ApplicationController
       bookmark.destroy
       redirect_to :back
     end
+  end
+
 
     def viewbookmarks
       if User.exists?(:id => params[:id])
@@ -124,4 +126,3 @@ class RecipesController < ApplicationController
       params.require(:recipe).permit(:name, :description)
     end
   end
-end
