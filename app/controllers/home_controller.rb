ed681147aa837bbb688bc3f305133d3c8a275c39
tantch <ei12019@fe.up.pt>
2015-11-29 18:45:53 +0000

    mini feed, apenas arrays certos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
 	def index
 		  @categories = Category.all
+			if current_user
+				@users = current_user.following_users
+				@users_id = @users.pluck(:id)
+				@comments = Comment.all.where(user_id: @users_id)
+			end
+
 	end
 
 end
